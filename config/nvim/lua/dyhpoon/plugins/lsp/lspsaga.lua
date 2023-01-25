5522b0ec7b9edca1b3d0727a5028d8c4908acb69
Darren Poon <dyhpoon@gmail.com>
2023-01-26 01:47:03 +0800

    upgrade dependencies

--- config/nvim/lua/dyhpoon/plugins/lsp/lspsaga.lua
+++ config/nvim/lua/dyhpoon/plugins/lsp/lspsaga.lua
@@ -5,9 +5,7 @@ if not saga_status then
   return
 end
 
-saga.init_lsp_saga({
-  -- keybinds for navigation in lspsaga window
-  move_in_saga = { prev = "<C-k>", next = "<C-j>" },
+saga.setup({
   -- use enter to open file with finder
   finder_action_keys = {
     open = "<CR>",
