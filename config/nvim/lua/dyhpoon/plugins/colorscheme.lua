b189440a404697aa0341119f6b4d7323bdbabda5
Darren Poon <dyhpoon@gmail.com>
2023-02-07 13:03:18 +0800

    update colorscheme to tokyonight-moon

--- config/nvim/lua/dyhpoon/plugins/colorscheme.lua
+++ config/nvim/lua/dyhpoon/plugins/colorscheme.lua
@@ -1,9 +1,8 @@
 return {
-  "bluz71/vim-nightfly-colors",
-  name = "nightfly",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    vim.cmd([[colorscheme nightfly]])
-  end,
+  {
+    "folke/tokyonight.nvim",
+    config = function()
+      vim.cmd([[colorscheme tokyonight-moon]])
+    end,
+  },
 }
