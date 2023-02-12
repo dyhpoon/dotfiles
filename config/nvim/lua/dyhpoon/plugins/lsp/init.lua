36c1774b5f28d06b469c201878c9642b1b4fe6c6
Darren Poon <dyhpoon@gmail.com>
2023-02-12 21:49:56 +0800

    revise null-ls

--- config/nvim/lua/dyhpoon/plugins/lsp/init.lua
+++ config/nvim/lua/dyhpoon/plugins/lsp/init.lua
@@ -173,34 +173,14 @@ return {
       local nls = require("null-ls")
       local formatting = nls.builtins.formatting -- to setup formatters
       local diagnostics = nls.builtins.diagnostics -- to setup linters
-      local code_actions = nls.builtins.code_actions
 
       return {
         sources = {
           formatting.stylua, -- lua formatter
           formatting.gofmt,
           formatting.golines,
-          diagnostics.golangci_lint,
-          code_actions.gitsigns,
+          diagnostics.golangci_lint
         },
-        on_attach = function(current_client, bufnr)
-          if current_client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-              group = augroup,
-              buffer = bufnr,
-              callback = function()
-                vim.lsp.buf.format({
-                  filter = function(client)
-                    --  only use null-ls for formatting instead of lsp server
-                    return client.name == "null-ls"
-                  end,
-                  bufnr = bufnr,
-                })
-              end,
-            })
-          end
-        end,
       }
     end,
   },
