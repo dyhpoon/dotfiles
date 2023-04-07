015364acb808167a54a1727e8a4fae447d344389
Darren Poon <dyhpoon@gmail.com>
2023-04-07 22:13:50 +0800

    revise lsp & remove lspsaga

--- config/nvim/lua/dyhpoon/plugins/lsp/init.lua
+++ config/nvim/lua/dyhpoon/plugins/lsp/init.lua
@@ -94,23 +94,19 @@ return {
         require("lspconfig")[server].setup(server_opts)
       end
 
-      -- temp fix for lspconfig rename
-      -- https://github.com/neovim/nvim-lspconfig/pull/2439
-      local mappings = require("mason-lspconfig.mappings.server")
-      if not mappings.lspconfig_to_package.lua_ls then
-        mappings.lspconfig_to_package.lua_ls = "lua-language-server"
-        mappings.package_to_lspconfig["lua-language-server"] = "lua_ls"
+      -- get all the servers that are available thourgh mason-lspconfig
+      local have_mason, mlsp = pcall(require, "mason-lspconfig")
+      local all_mslp_servers = {}
+      if have_mason then
+        all_mslp_servers = vim.tbl_keys(require("mason-lspconfig.mappings.server").lspconfig_to_package)
       end
-
-      local mlsp = require("mason-lspconfig")
-      local available = mlsp.get_available_servers()
 
       local ensure_installed = {} ---@type string[]
       for server, server_opts in pairs(servers) do
         if server_opts then
           server_opts = server_opts == true and {} or server_opts
           -- run manual setup if mason=false or if this is a server that cannot be installed with mason-lspconfig
-          if server_opts.mason == false or not vim.tbl_contains(available, server) then
+          if server_opts.mason == false or not vim.tbl_contains(all_mslp_servers, server) then
             setup(server)
           else
             ensure_installed[#ensure_installed + 1] = server
@@ -118,8 +114,10 @@ return {
         end
       end
 
-      require("mason-lspconfig").setup({ ensure_installed = ensure_installed })
-      require("mason-lspconfig").setup_handlers({ setup })
+      if have_mason then
+        mlsp.setup({ ensure_installed = ensure_installed })
+        mlsp.setup_handlers({ setup })
+      end
     end,
   },
 
@@ -150,17 +148,6 @@ return {
     },
     dependencies = {
       "williamboman/mason.nvim",
-    },
-  },
-
-  {
-    "glepnir/lspsaga.nvim",
-    event = "BufRead",
-    config = function()
-      require("lspsaga").setup({})
-    end,
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
     },
   },
 
