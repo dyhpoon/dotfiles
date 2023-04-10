50d59d79189ab8d1a3259bf784ad9e285203af63
Darren Poon <dyhpoon@gmail.com>
2023-04-10 19:26:06 +0800

    revise toggle term config

--- config/nvim/lua/dyhpoon/plugins/toggleterm.lua
+++ config/nvim/lua/dyhpoon/plugins/toggleterm.lua
@@ -10,24 +10,24 @@ return {
         elseif term.direction == "vertical" then
           return vim.o.columns * 0.4
         end
-      end
+      end,
     },
     config = function(_, opts)
       require("toggleterm").setup(opts)
       function _G.set_terminal_keymaps()
-        local opts = {buffer = 0}
-        vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
-        vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
-        vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-        vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-        vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-        vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
-        vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+        local options = { buffer = 0 }
+        vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], options)
+        vim.keymap.set("t", "jk", [[<C-\><C-n>]], options)
+        vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], options)
+        vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], options)
+        vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], options)
+        vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], options)
+        vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], options)
       end
       -- if you only want these mappings for toggle term use term://*toggleterm#* instead
-      vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+      vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
 
-      local Terminal = require('toggleterm.terminal').Terminal
+      local Terminal = require("toggleterm.terminal").Terminal
       local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, direction = "float" })
       local htop = Terminal:new({ cmd = "htop", hidden = true, direction = "float" })
 
