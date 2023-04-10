c84451bb63be14eda1e1adad8c8be05bbfce533d
Darren Poon <dyhpoon@gmail.com>
2023-04-10 23:29:44 +0800

    show dapui when debugger is on

--- config/nvim/lua/dyhpoon/plugins/dap.lua
+++ config/nvim/lua/dyhpoon/plugins/dap.lua
@@ -20,7 +20,17 @@ return {
       { "<leader>dt", ":lua require'dap'.terminate()<cr>", desc = "Terminate" },
     },
     config = function()
-      require("dapui").setup()
+      local dap, dapui = require("dap"), require("dapui")
+      dapui.setup()
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
     end,
   },
   {
